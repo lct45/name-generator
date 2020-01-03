b6089436ad33039fc333a5c66190b4b184030fe1
lct45 <leah.thomas144@gmail.com>
2020-01-03 16:43:42 -0600

    brute force scraping

--- personalprojects/name-generator/generator.py
+++ personalprojects/name-generator/generator.py
@@ -1,8 +1,8 @@
 import requests
 from lxml import html
 
-USERNAME = "<USERNAME>"
-PASSWORD = "<PASSWORD>"
+USERNAME = "leah.thomas"
+PASSWORD = "Gj18f20L$HUt"
 LOGIN_URL = "https://ident.familysearch.org/cis-web/oauth2/v3/authorization?client_secret=qgyA0c1Q4s%2FFjVlEFu5K5yjq2vlXsD6lc40vIN1RS8O%2BtB7ymm%2Fhu0k0iMMTaHKYkbOuZqgn32UOnBpxYJA8wn0hguF6AuoqMsoXlFdtnhqDGWsS4t23UN0yp0cxVO5sMLL8Vg5dyaK0c2y9COCzQnSB0U2aLmIRqkdUZYxoAEI7lELOJ1wPPh9zMcxJfsuy9UAewy4TAm1lj%2BUACztlWuMht%2BVKNU5gKE53jNaevBKZTY9%2B%2FRHW6ypIvS3Fp%2BID67E6mzkugRU5NIrAEV4v%2FLU8DXC5%2FxP0Vmc37VZNnWGomLQOBdfEuBij9Yo6xHinIHjgqGQ%2Fcd%2BkQwWWI%2BPOhg%3D%3D&response_type=code&redirect_uri=https%3A%2F%2Fwww.familysearch.org%2Fauth%2Ffamilysearch%2Fcallback&state=%2F&client_id=3Z3L-Z4GK-J7ZS-YT3Z-Q4KY-YN66-ZX5K-176R"
 
 def main():
